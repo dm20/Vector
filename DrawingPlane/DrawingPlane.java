8589239121613ca5477fdf2fe005a969a7bc426d
Daniel McGrath <demcgrath2@gmail.com>
2017-08-18 21:37:54 -0700

    Inserted comment about compilation
    -instruction to compile vector class first

--- DrawingPlane/DrawingPlane.java
+++ DrawingPlane/DrawingPlane.java
@@ -14,6 +14,7 @@ import java.awt.MouseInfo.*;
 Draw 2D lines on a plane by clicking the start and end points.
 
 To compile and run the program, enter these two lines in Terminal (mac OS X): 
+          javac Vector.java
           javac DrawingPlane.java
           java DrawingPlane
 */
